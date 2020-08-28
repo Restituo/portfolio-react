d8f7fccc061a943da8239294919c8054ed0c1e88
Restituo <rcc999@comcast.net>
2020-08-28 11:24:01 -0700

    add base outline

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import './Style.css';
+import Welcome from './Containers/Welcome';
+import AboutMe from './Containers/AboutMe';
+import Projects from './Containers/Projects';
+import Resume from './Containers/Resume';
+import Connect from './Containers/Connect';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  //animation
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  //layout
+  return(
+    <div>
+      <Navbar/>
+      <Welcome/>
+      <AboutMe/>
+      <Projects/>
+      <Resume/>
+      <Connect/>
+      <Footer/>
     </div>
   );
 }
